6bcf69c67aee3206120d548c4d33c307fbc83812
l96997566-creator <l96997566@gmail.com>
2026-05-28 15:21:55 +0900

    최종~git add calc.cpp

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,6 +1,15 @@
-﻿#include <iostream>
+﻿#include "add.h"
+#include "sub.h"
+#include "mul.h"
+#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = add(10, 2);
+    int b = sub(10, 2);
+    int c = mul(10, 2);
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
 }
